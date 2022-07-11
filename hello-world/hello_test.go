a00e3a884efed1aa620930946fa2e8a8cfe5ed93
hemanta212 <sharmahemanta.212@gmail.com>
2022-07-11 15:07:53 +0545

    Extend hello world

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,8 +3,9 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, World!"
+	got := Hello("Chris")
+	want := "Hello, Chris!"
+
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
