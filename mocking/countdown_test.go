733aad3cd0fa25b5c76f8047c0e0d89066224e09
hemanta212 <sharmahemanta.212@gmail.com>
2022-07-15 13:32:44 +0545

    Mocking: Countdown timer

---
+++ mocking/countdown_test.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bytes"
+	"reflect"
+	"testing"
+	"time"
+)
+
+func TestCoundown(t *testing.T) {
+	t.Run("Verify output", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		sleeper := &SpyCountdownOperations{}
+
+		Countdown(buffer, sleeper)
+
+		got := buffer.String()
+		want := `3
+2
+1
+Go!`
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("Sleep before every print", func(t *testing.T) {
+		spySleepPrinter := &SpyCountdownOperations{}
+		Countdown(spySleepPrinter, spySleepPrinter)
+
+		want := []string{
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+		got := spySleepPrinter.Calls
+		if !reflect.DeepEqual(want, got) {
+			t.Errorf("wanted calls %v got %v", want, got)
+		}
+	})
+
+}
+
+func TestConfigurableSleeper(t *testing.T) {
+	sleepTime := 5 * time.Second
+
+	spyTime := &SpyTime{}
+	sleeper := ConfigurableSleeper{sleepTime, spyTime.Sleep}
+	sleeper.Sleep()
+
+	if spyTime.durationSlept != sleepTime {
+		t.Errorf("should have slept for %v but slept for %v", sleepTime, spyTime.durationSlept)
+	}
+}
+
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const write = "write"
+const sleep = "sleep"
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
