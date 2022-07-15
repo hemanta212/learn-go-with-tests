7c6772c3f1fa2b5c536c14207956130be552bed0
hemanta212 <sharmahemanta.212@gmail.com>
2022-07-15 09:12:31 +0545

    Dependency Injection: Add io.Writer usage hello world

---
+++ dependency_injection/di.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s!", name)
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	Greet(os.Stdout, "Chris")
+	// Runs a http server at localhost:5001 that prints hello world
+	// log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreeterHandler)))
+}
