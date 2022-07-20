d14e4b3c218b20d348803eeaa6d2efa2ecf44102
hemanta212 <sharmahemanta.212@gmail.com>
2022-07-20 06:47:04 +0545

    Context: Cancelling http Server Operations with Context Propagation

---
+++ context/context.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+)
+
+func Server(store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		data, err := store.Fetch(r.Context())
+		if err != nil {
+			return // TODO: log error etc
+		}
+		fmt.Fprintf(w, data)
+	}
+}
+
+type Store interface {
+	Fetch(ctx context.Context) (string, error)
+}
